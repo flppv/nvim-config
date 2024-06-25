deb877410403169b6d6ae06f8fc24ed142571544
Anton <flppv8@gmail.com>
2024-06-26 01:56:22 +0700

    create starter screen, improve telescope mappings

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,60 +1,126 @@
-local M = {
-	"echasnovski/mini.nvim",
-	version = "*",
-	event = "VeryLazy",
-	config = function()
-		require("mini.cursorword").setup({ delay = 0 })
-		require("mini.pairs").setup()
-		require("mini.comment").setup()
+local config = {
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.cursorword").setup({ delay = 0 })
+			-- require("mini.pairs").setup() -- Uncomment if needed
+			require("mini.comment").setup()
 
-		local miniclue = require("mini.clue")
-		require("mini.clue").setup({
-			triggers = {
-				-- Leader triggers
-				{ mode = "n", keys = "<Leader>" },
-				{ mode = "x", keys = "<Leader>" },
+			local miniclue = require("mini.clue")
+			require("mini.clue").setup({
+				triggers = {
+					-- Leader triggers
+					{ mode = "n", keys = "<Leader>" },
+					{ mode = "x", keys = "<Leader>" },
 
-				-- Built-in completion
-				{ mode = "i", keys = "<C-x>" },
+					-- Built-in completion
+					{ mode = "i", keys = "<C-x>" },
 
-				-- `g` key
-				{ mode = "n", keys = "g" },
-				{ mode = "x", keys = "g" },
+					-- `g` key
+					{ mode = "n", keys = "g" },
+					{ mode = "x", keys = "g" },
 
-				-- Marks
-				{ mode = "n", keys = "'" },
-				{ mode = "n", keys = "`" },
-				{ mode = "x", keys = "'" },
-				{ mode = "x", keys = "`" },
+					-- Marks
+					{ mode = "n", keys = "'" },
+					{ mode = "n", keys = "`" },
+					{ mode = "x", keys = "'" },
+					{ mode = "x", keys = "`" },
 
-				-- Registers
-				{ mode = "n", keys = '"' },
-				{ mode = "x", keys = '"' },
-				{ mode = "i", keys = "<C-r>" },
-				{ mode = "c", keys = "<C-r>" },
+					-- Registers
+					{ mode = "n", keys = '"' },
+					{ mode = "x", keys = '"' },
+					{ mode = "i", keys = "<C-r>" },
+					{ mode = "c", keys = "<C-r>" },
 
-				-- Window commands
-				{ mode = "n", keys = "<C-w>" },
+					-- Window commands
+					{ mode = "n", keys = "<C-w>" },
 
-				-- `z` key
-				{ mode = "n", keys = "z" },
-				{ mode = "x", keys = "z" },
-			},
-
-			clues = {
-				-- Enhance this by adding descriptions for <Leader> mapping groups
-				miniclue.gen_clues.builtin_completion(),
-				miniclue.gen_clues.g(),
-				miniclue.gen_clues.marks(),
-				miniclue.gen_clues.registers(),
-				miniclue.gen_clues.windows(),
-				miniclue.gen_clues.z(),
-			},
-			window = {
-				delay = 200,
-			},
-		})
-	end,
+					-- `z` key
+					{ mode = "n", keys = "z" },
+					{ mode = "x", keys = "z" },
+				},
+				clues = {
+					miniclue.gen_clues.builtin_completion(),
+					miniclue.gen_clues.g(),
+					miniclue.gen_clues.marks(),
+					miniclue.gen_clues.registers(),
+					miniclue.gen_clues.windows(),
+					miniclue.gen_clues.z(),
+				},
+				window = {
+					delay = 200,
+				},
+			})
+		end,
+		event = "VimEnter",
+		version = "*",
+	},
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.starter").setup({
+				autoopen = true,
+				items = {
+					{
+						name = "Open file",
+						action = "Telescope find_files",
+						section = "Telescope",
+					},
+					{
+						name = "Recent files",
+						action = "Telescope oldfiles",
+						section = "Telescope",
+					},
+					{
+						name = "Grep text",
+						action = "Telescope live_grep",
+						section = "Telescope",
+					},
+					{
+						name = "File Browser",
+						action = "Oil",
+						section = "Telescope",
+						keymap = "fb",
+					},
+					{
+						name = "Update plugins",
+						action = "Lazy update",
+						section = "Actions",
+					},
+					{
+						name = "Quit",
+						action = "qa",
+						section = "Actions",
+					},
+					{
+						name = "Enter config directory",
+						action = "Oil ~/.config/nvim",
+						section = "Actions",
+					},
+				},
+				content_hooks = {
+					require("mini.starter").gen_hook.adding_bullet(),
+					require("mini.starter").gen_hook.aligning("center", "center"),
+				},
+				footer = "Write code, create magic",
+				header = table.concat({
+					"      .---.        .-----------",
+					"     /     \\  __  /    ------",
+					"    / /     \\(  )/    -----",
+					"   //////   ' \\/ `   ---",
+					"  //// / // :    : ---",
+					" // /   /  /`    '--",
+					"//          //..\\\\",
+					"       ====UU====UU====",
+					"           '//'||\\\\`",
+					"             ''``",
+				}, "\n"),
+			})
+		end,
+		event = "BufEnter",
+		lazy = true,
+		version = "*",
+	},
 }
 
-return M
+return config
