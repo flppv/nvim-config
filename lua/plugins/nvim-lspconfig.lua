e889e7bc2937f5025258b9da99b1a67fbb77247c
Anton <flppv8@gmail.com>
2025-10-23 17:32:07 +0700

    bump versions, ai fixes for deprecation warnings, add avante for possible future use

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,139 +1,136 @@
 local M = {
-	"neovim/nvim-lspconfig",
-	lazy = true,
-	config = function()
-		local lspconfig = require("lspconfig")
+  "neovim/nvim-lspconfig",
+  lazy = true,
+  config = function()
+    local lsp = vim.lsp
+    local emmet_capabilities = vim.lsp.protocol.make_client_capabilities()
+    emmet_capabilities.textDocument.completion.completionItem.snippetSupport = true
+    local opts = {
+      noremap = true,
+      silent = true,
+    }
+    -- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
-		-- local configs = require('lspconfig/configs')
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.textDocument.completion.completionItem.snippetSupport = true
-		local opts = {
-			noremap = true,
-			silent = true,
-		}
-		-- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+    -- Use an on_attach function to only map the following keys
+    -- after the language server attaches to the current buffer
+    local on_attach = function(client, bufnr)
+      -- Enable completion triggered by <c-x><c-o>
+      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-		-- Use an on_attach function to only map the following keys
-		-- after the language server attaches to the current buffer
-		local on_attach = function(client, bufnr)
-			-- Enable completion triggered by <c-x><c-o>
-			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+      -- Mappings.
+      -- See `:help vim.lsp.*` for documentation on any of the below functions
+      local bufopts = {
+        noremap = true,
+        silent = true,
+        buffer = bufnr,
+      }
+      vim.keymap.set("n", "<space>ft", function()
+        vim.lsp.buf.format({
+          async = true,
+        })
+      end, bufopts)
+    end
 
-			-- Mappings.
-			-- See `:help vim.lsp.*` for documentation on any of the below functions
-			local bufopts = {
-				noremap = true,
-				silent = true,
-				buffer = bufnr,
-			}
-			vim.keymap.set("n", "<space>ft", function()
-				vim.lsp.buf.format({
-					async = true,
-				})
-			end, bufopts)
-		end
+    local lsp_flags = {
+      debounce_text_changes = 150,
+    }
 
-		local lsp_flags = {
-			debounce_text_changes = 150,
-		}
+    local signs = {
+      Error = " ",
+      Warn = " ",
+      Hint = " ",
+      Info = " ",
+    }
 
-		local signs = {
-			Error = " ",
-			Warn = " ",
-			Hint = " ",
-			Info = " ",
-		}
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, {
+        text = icon,
+        texthl = hl,
+        numhl = hl,
+      })
+    end
 
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, {
-				text = icon,
-				texthl = hl,
-				numhl = hl,
-			})
-		end
+    local config = {
+      virtual_text = true,
+    }
 
-		local config = {
-			virtual_text = true,
-		}
+    vim.diagnostic.config(config)
 
-		vim.diagnostic.config(config)
+    -- require("lspconfig").biome.setup({ autostart = false })
 
-		-- lspconfig["biome"].setup({})
+    local lsp_format = require("lsp-format")
+    lsp_format.setup({})
 
-		require("lsp-format").setup({})
+    local servers = {
+      astro = {
+        on_attach = lsp_format.on_attach,
+      },
+      svelte = {
+        on_attach = on_attach,
+        flags = lsp_flags,
+      },
+      tailwindcss = {
+        on_attach = on_attach,
+        flags = lsp_flags,
+      },
+      lua_ls = {
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+              checkThirdParty = false,
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      },
+      emmet_ls = {
+        -- on_attach = on_attach,
+        capabilities = emmet_capabilities,
+        filetypes = { "html", "astro", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      },
+    }
 
-		lspconfig["astro"].setup({
-			on_attach = require("lsp-format").on_attach,
-		})
+    require("typescript-tools").setup({
+      settings = {
+        tsserver_plugins = {
+          -- for TypeScript v4.9+
+          "@styled/typescript-styled-plugin",
+          -- or for older TypeScript versions
+          -- "typescript-styled-plugin",
+        },
+      },
+    })
 
-		-- lspconfig["tsserver"].setup({
-		-- 	on_attach = on_attach,
-		-- 	flags = lsp_flags,
-		-- })
-
-		lspconfig["svelte"].setup({
-			on_attach = on_attach,
-			flags = lsp_flags,
-		})
-
-		require("typescript-tools").setup({
-			settings = {
-				tsserver_plugins = {
-					-- for TypeScript v4.9+
-					"@styled/typescript-styled-plugin",
-					-- or for older TypeScript versions
-					-- "typescript-styled-plugin",
-				},
-			},
-		})
-
-		lspconfig.tailwindcss.setup({
-			on_attach = on_attach,
-			flags = lsp_flags,
-		})
-
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					runtime = {
-						-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = { "vim" },
-						checkThirdParty = false,
-					},
-					workspace = {
-						-- Make the server aware of Neovim runtime files
-						library = vim.api.nvim_get_runtime_file("", true),
-						checkThirdParty = false,
-					},
-					-- Do not send telemetry data containing a randomized but unique identifier
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		})
-
-		lspconfig.emmet_ls.setup({
-			-- on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "html", "astro", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
-			init_options = {
-				html = {
-					options = {
-						-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-						["bem.enabled"] = true,
-					},
-				},
-			},
-		})
-	end,
+    for name, config in pairs(servers) do
+      lsp.config(name, config)
+      lsp.enable(name)
+    end
+  end,
 }
 return M
